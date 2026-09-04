f5d9ed72f3b8136952da47e46483fa5d8722bfbb
method-bracket <aleksy.gre@gmail.com>
2026-09-04 17:04:49 +0200

    Fix query in ArtskoolRepository

--- src/main/java/com/example/artskool_back/repository/ArtskoolRepository.java
+++ src/main/java/com/example/artskool_back/repository/ArtskoolRepository.java
@@ -2,15 +2,22 @@ package com.example.artskool_back.repository;
 
 import com.example.artskool_back.entity.Event;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.NativeQuery;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
 public interface ArtskoolRepository extends JpaRepository<Event, Long> {
-    @Query("SELECT * FROM events WHERE end_datetime > NOW() " +
-            "ORDER BY CASE WHEN start_datetime <= NOW() THEN 0 ELSE 1 END, " +
-            "CASE WHEN start_datetime <= NOW() THEN start_datetime END DESC, " +
-            "CASE WHEN start_datetime > NOW() THEN start_datetime END ASC " +
-            "LIMIT 1;")
+    @NativeQuery(value = "SELECT * FROM event e " +
+            "WHERE e.date_time_to > CURRENT_TIMESTAMP " +
+            "ORDER BY " +
+            "CASE WHEN e.date_time_from <= CURRENT_TIMESTAMP THEN 0 " +
+            "ELSE 1 " +
+            "END, " +
+            "CASE WHEN e.date_time_from <= CURRENT_TIMESTAMP THEN e.date_time_from " +
+            "END DESC, " +
+            "CASE WHEN e.date_time_from > CURRENT_TIMESTAMP THEN e.date_time_from " +
+            "END ASC " +
+            "LIMIT 1")
     Optional<Event> findNearestUpcomingEvent();
 }
